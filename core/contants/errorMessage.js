139f9d11c91a3abde181f8291129b665f30f4d03
kimhuy011199 <kimhuy011199@gmail.com>
2022-07-06 21:42:10 +0700

    Update title error message

--- core/contants/errorMessage.js
+++ core/contants/errorMessage.js
@@ -7,16 +7,16 @@ const ERROR_MESSAGE = {
   NOTIFICATION_NOT_FOUND: 'Notification not found',
   TAG_NOT_FOUND: 'Tag not found',
   REQUIRED_FIELD: 'Required fields error',
-  TITLE_EXIST: 'Title already exists',
+  TITLE_EXIST: 'A question with that title already exists',
   EMAIL_EXIST: 'Email already exists',
   NAME_EXIST: 'Name already exists',
   DISPLAYNAME_EXIST: 'Display name already exists',
   INVALID_INPUT: 'Invalid input',
   INVALID_EMAIL_PASSOWRD: 'Invalid email or password',
   INVALID_PASSOWRD: 'Invalid password',
-  CLOUDINARY_ERROR: 'Cannot upload image to Cloudinary'
-}
+  CLOUDINARY_ERROR: 'Cannot upload image to Cloudinary',
+};
 
 module.exports = {
-  ERROR_MESSAGE
+  ERROR_MESSAGE,
 };
